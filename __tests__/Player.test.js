551ed064361056e571e658762296adfee225c145
smckelli <smckelli@gmail.com>
2022-05-01 15:39:34 -0500

    Test and build player methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -22,6 +22,12 @@ test('creates a player object', () => {
     );
 });
 
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
 test("gets player's stats as an object", () => {
     const player = new Player('Dave');
   
@@ -39,4 +45,27 @@ test("gets player's stats as an object", () => {
     player.inventory = [];
   
     expect(player.getInventory()).toEqual(false);
+  });
+
+  test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
+  });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
